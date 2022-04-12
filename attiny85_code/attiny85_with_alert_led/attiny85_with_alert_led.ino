6368eb12b23d2a117f11ad497ffbf1033757ce67
Suryah07 <suryahdhamodharan@gmail.com>
2022-04-12 23:21:14 +0530

    started upgrading for other sensors

--- attiny85_code/attiny85_with_alert_led/attiny85_with_alert_led.ino
+++ attiny85_code/attiny85_with_alert_led/attiny85_with_alert_led.ino
@@ -1,20 +1,21 @@
 //This code is to use with DS1302 RTC module, it permits you to setup the actual time and date
 //And you can visualize them on the serial monitor
+//Attach an led to pin PB2 for alert messages
 //This code is a modified version of the code provided in virtuabotixRTC library
 //Refer to https://Surtrtech.com for more information
 
 #include <virtuabotixRTC.h> //Library used
-
-//Wiring SCLK -> 6, I/O -> 7, CE -> 8
 //Or CLK -> PB4  , DAT -> PB3, Reset -> PB1
-#define relay PB0
-#define alertlight PB1
-#define watertime 60000 //time of motor to be on in milliseconds...
+
+
+#define relay PB0 //Relay is connected to PB0
+#define alertlight PB2 //Light for alerting for any errors
+#define watertime 60000 //time of motor to be ON in milliseconds...
 
 
 virtuabotixRTC myRTC(PB4, PB3, PB1); //If you change the wiring change the pins here also
 
-int hourarr[] = {11,12,13,14,15,19};
+int hourarr[] = {11,12,13,14,15,19}; //24 hour time formatt
 int arrlength = sizeof(hourarr)/sizeof(int);
 /*
 void alert_no_connection()
@@ -56,14 +57,15 @@ void startwater()
 
 void setup() {
   pinMode(relay,OUTPUT);
+  pinMode(alertlight,OUTPUT);
 // seconds, minutes, hours, day of the week, day of the month, month, year
 //myRTC.setDS1302Time(00, 23, 18, 1, 11, 04, 2022); //Here you write your actual time/date as shown above 
 }
 
-void loop() {
- 
+void loop() 
+{ 
  myRTC.updateTime();
-
+ 
  if(wateringtime(myRTC))
  {
   startwater();
---
+++ attiny85_code/attiny85_with_humidity_sensor/attiny85_with_humidity_sensor.ino
@@ -0,0 +1,57 @@
+//This code is to use with DS1302 RTC module, it permits you to setup the actual time and date
+//And you can visualize them on the serial monitor
+//Attach an dht11 humidity sensor
+//This code is a modified version of the code provided in virtuabotixRTC library
+//Refer to https://Surtrtech.com for more information
+
+#include <virtuabotixRTC.h> //Library used
+
+//Or CLK -> PB4  , DAT -> PB3, Reset -> PB1
+
+
+#define relay PB0 //Relay is connected to PB0
+#define sensordata PB2 //Light for alerting for any errors
+#define watertime 60000 //time of motor to be ON in milliseconds...
+
+
+virtuabotixRTC myRTC(PB4, PB3, PB1); //If you change the wiring change the pins here also
+
+int hourarr[] = {11,12,13,14,15,19}; //24 hour time formatt
+int arrlength = sizeof(hourarr)/sizeof(int);
+
+bool wateringtime(virtuabotixRTC tim)
+{
+  for(int i=0;i<arrlength;i++)
+  {
+    if(tim.hours == hourarr[i] and (tim.minutes==01 or tim.minutes==02))
+    {
+      return true;      
+    }
+  }
+  return false;
+}
+
+void startwater()
+{
+  digitalWrite(relay,HIGH);
+  delay(watertime);
+  digitalWrite(relay,LOW);
+}
+
+void setup() {
+  pinMode(relay,OUTPUT);
+  pinMode(alertlight,OUTPUT);
+// seconds, minutes, hours, day of the week, day of the month, month, year
+//myRTC.setDS1302Time(00, 23, 18, 1, 11, 04, 2022); //Here you write your actual time/date as shown above 
+}
+
+void loop() 
+{ 
+ myRTC.updateTime();
+ 
+ if(wateringtime(myRTC))
+ {
+  startwater();
+ }
+ delay(1000);
+}
